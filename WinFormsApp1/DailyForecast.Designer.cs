4e85a5b76e773072afa8f2a647ec0b1a80124fba
Weirdoliam <Weirdoliam@gmail.com>
2023-05-05 18:51:59 +1200

    Major overhaul

--- WinFormsApp1/DailyForecast.Designer.cs
+++ WinFormsApp1/DailyForecast.Designer.cs
@@ -32,24 +32,9 @@
             labelCurrDay = new System.Windows.Forms.Label();
             listBoxModelHouses = new System.Windows.Forms.ListBox();
             canvas = new System.Windows.Forms.Panel();
-            label2 = new System.Windows.Forms.Label();
-            label3 = new System.Windows.Forms.Label();
-            label4 = new System.Windows.Forms.Label();
-            label5 = new System.Windows.Forms.Label();
-            label6 = new System.Windows.Forms.Label();
-            checkBoxAverage = new System.Windows.Forms.CheckBox();
             labelPanel = new System.Windows.Forms.Panel();
             checkBoxSmooth = new System.Windows.Forms.CheckBox();
-            panelElectric = new System.Windows.Forms.Panel();
-            panelMains = new System.Windows.Forms.Panel();
-            panelOther = new System.Windows.Forms.Panel();
-            label7 = new System.Windows.Forms.Label();
-            label8 = new System.Windows.Forms.Label();
-            label9 = new System.Windows.Forms.Label();
-            label10 = new System.Windows.Forms.Label();
             labelAverageDaily = new System.Windows.Forms.Label();
-            radioButtonOne = new System.Windows.Forms.RadioButton();
-            radioButtonAll = new System.Windows.Forms.RadioButton();
             listBoxGenerators = new System.Windows.Forms.ListBox();
             labelSunrise = new System.Windows.Forms.Label();
             labelSunset = new System.Windows.Forms.Label();
@@ -65,6 +50,7 @@
             labelGenerate = new System.Windows.Forms.Label();
             label14 = new System.Windows.Forms.Label();
             labelNet = new System.Windows.Forms.Label();
+            label10 = new System.Windows.Forms.Label();
             SuspendLayout();
             // 
             // labelCurrDay
@@ -91,76 +77,23 @@
             // 
             // canvas
             // 
+            canvas.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+            canvas.AutoSize = true;
             canvas.BackColor = System.Drawing.SystemColors.ControlLight;
-            canvas.Location = new System.Drawing.Point(251, 97);
+            canvas.Location = new System.Drawing.Point(47, 190);
             canvas.Name = "canvas";
-            canvas.Size = new System.Drawing.Size(674, 300);
+            canvas.Size = new System.Drawing.Size(881, 332);
             canvas.TabIndex = 4;
             canvas.Paint += panel1_Paint;
             canvas.MouseClick += canvas_MouseClick;
             // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Location = new System.Drawing.Point(234, 402);
-            label2.Name = "label2";
-            label2.Size = new System.Drawing.Size(51, 15);
-            label2.TabIndex = 5;
-            label2.Text = "12:00am";
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Location = new System.Drawing.Point(903, 402);
-            label3.Name = "label3";
-            label3.Size = new System.Drawing.Size(52, 15);
-            label3.TabIndex = 6;
-            label3.Text = "11:30pm";
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Location = new System.Drawing.Point(572, 402);
-            label4.Name = "label4";
-            label4.Size = new System.Drawing.Size(52, 15);
-            label4.TabIndex = 7;
-            label4.Text = "12:00pm";
-            // 
-            // label5
-            // 
-            label5.AutoSize = true;
-            label5.Location = new System.Drawing.Point(406, 402);
-            label5.Name = "label5";
-            label5.Size = new System.Drawing.Size(45, 15);
-            label5.TabIndex = 8;
-            label5.Text = "6:00am";
-            // 
-            // label6
-            // 
-            label6.AutoSize = true;
-            label6.Location = new System.Drawing.Point(745, 402);
-            label6.Name = "label6";
-            label6.Size = new System.Drawing.Size(46, 15);
-            label6.TabIndex = 9;
-            label6.Text = "6:00pm";
-            // 
-            // checkBoxAverage
-            // 
-            checkBoxAverage.AutoSize = true;
-            checkBoxAverage.Location = new System.Drawing.Point(27, 94);
-            checkBoxAverage.Name = "checkBoxAverage";
-            checkBoxAverage.Size = new System.Drawing.Size(98, 19);
-            checkBoxAverage.TabIndex = 10;
-            checkBoxAverage.Text = "Daily Average";
-            checkBoxAverage.UseVisualStyleBackColor = true;
-            checkBoxAverage.CheckedChanged += checkBoxAverage_CheckedChanged;
-            // 
             // labelPanel
             // 
+            labelPanel.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left;
             labelPanel.BackColor = System.Drawing.SystemColors.Control;
-            labelPanel.Location = new System.Drawing.Point(211, 97);
+            labelPanel.Location = new System.Drawing.Point(8, 190);
             labelPanel.Name = "labelPanel";
-            labelPanel.Size = new System.Drawing.Size(40, 300);
+            labelPanel.Size = new System.Drawing.Size(40, 332);
             labelPanel.TabIndex = 11;
             labelPanel.Paint += panel1_Paint_1;
             // 
@@ -175,96 +108,15 @@
             checkBoxSmooth.UseVisualStyleBackColor = true;
             checkBoxSmooth.CheckedChanged += checkBoxSmooth_CheckedChanged;
             // 
-            // panelElectric
-            // 
-            panelElectric.Location = new System.Drawing.Point(8, 195);
-            panelElectric.Name = "panelElectric";
-            panelElectric.Size = new System.Drawing.Size(200, 40);
-            panelElectric.TabIndex = 13;
-            // 
-            // panelMains
-            // 
-            panelMains.Location = new System.Drawing.Point(8, 264);
-            panelMains.Name = "panelMains";
-            panelMains.Size = new System.Drawing.Size(200, 40);
-            panelMains.TabIndex = 14;
-            // 
-            // panelOther
-            // 
-            panelOther.Location = new System.Drawing.Point(8, 333);
-            panelOther.Name = "panelOther";
-            panelOther.Size = new System.Drawing.Size(200, 40);
-            panelOther.TabIndex = 15;
-            // 
-            // label7
-            // 
-            label7.AutoSize = true;
-            label7.Location = new System.Drawing.Point(8, 177);
-            label7.Name = "label7";
-            label7.Size = new System.Drawing.Size(127, 15);
-            label7.TabIndex = 16;
-            label7.Text = "Electric Consumption: ";
-            // 
-            // label8
-            // 
-            label8.AutoSize = true;
-            label8.Location = new System.Drawing.Point(8, 246);
-            label8.Name = "label8";
-            label8.Size = new System.Drawing.Size(143, 15);
-            label8.TabIndex = 17;
-            label8.Text = "Mains Gas Consumption: ";
-            // 
-            // label9
-            // 
-            label9.AutoSize = true;
-            label9.Location = new System.Drawing.Point(8, 315);
-            label9.Name = "label9";
-            label9.Size = new System.Drawing.Size(119, 15);
-            label9.TabIndex = 18;
-            label9.Text = "Other Consumption: ";
-            // 
-            // label10
-            // 
-            label10.AutoSize = true;
-            label10.Location = new System.Drawing.Point(8, 381);
-            label10.Name = "label10";
-            label10.Size = new System.Drawing.Size(197, 15);
-            label10.TabIndex = 19;
-            label10.Text = "Average Daily Comsumption (kWh):";
-            // 
             // labelAverageDaily
             // 
             labelAverageDaily.AutoSize = true;
             labelAverageDaily.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            labelAverageDaily.Location = new System.Drawing.Point(64, 408);
+            labelAverageDaily.Location = new System.Drawing.Point(131, 137);
             labelAverageDaily.Name = "labelAverageDaily";
             labelAverageDaily.Size = new System.Drawing.Size(63, 25);
             labelAverageDaily.TabIndex = 20;
             labelAverageDaily.Text = "0 kWh";
-            // 
-            // radioButtonOne
-            // 
-            radioButtonOne.AutoSize = true;
-            radioButtonOne.Location = new System.Drawing.Point(12, 69);
-            radioButtonOne.Name = "radioButtonOne";
-            radioButtonOne.Size = new System.Drawing.Size(105, 19);
-            radioButtonOne.TabIndex = 21;
-            radioButtonOne.TabStop = true;
-            radioButtonOne.Text = "Current Item(s)";
-            radioButtonOne.UseVisualStyleBackColor = true;
-            radioButtonOne.CheckedChanged += radioButton1_CheckedChanged;
-            // 
-            // radioButtonAll
-            // 
-            radioButtonAll.AutoSize = true;
-            radioButtonAll.Location = new System.Drawing.Point(12, 44);
-            radioButtonAll.Name = "radioButtonAll";
-            radioButtonAll.Size = new System.Drawing.Size(71, 19);
-            radioButtonAll.TabIndex = 22;
-            radioButtonAll.TabStop = true;
-            radioButtonAll.Text = "All Items";
-            radioButtonAll.UseVisualStyleBackColor = true;
-            radioButtonAll.CheckedChanged += radioButton2_CheckedChanged;
             // 
             // listBoxGenerators
             // 
@@ -299,7 +151,7 @@
             // checkBoxArrayTotal
             // 
             checkBoxArrayTotal.AutoSize = true;
-            checkBoxArrayTotal.Location = new System.Drawing.Point(27, 119);
+            checkBoxArrayTotal.Location = new System.Drawing.Point(8, 44);
             checkBoxArrayTotal.Name = "checkBoxArrayTotal";
             checkBoxArrayTotal.Size = new System.Drawing.Size(108, 19);
             checkBoxArrayTotal.TabIndex = 26;
@@ -341,7 +193,7 @@
             // checkBoxFactoyLoad
             // 
             checkBoxFactoyLoad.AutoSize = true;
-            checkBoxFactoyLoad.Location = new System.Drawing.Point(8, 144);
+            checkBoxFactoyLoad.Location = new System.Drawing.Point(8, 68);
             checkBoxFactoyLoad.Name = "checkBoxFactoyLoad";
             checkBoxFactoyLoad.Size = new System.Drawing.Size(137, 19);
             checkBoxFactoyLoad.TabIndex = 30;
@@ -353,7 +205,7 @@
             // 
             label1.AutoSize = true;
             label1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            label1.Location = new System.Drawing.Point(8, 448);
+            label1.Location = new System.Drawing.Point(337, 94);
             label1.Name = "label1";
             label1.Size = new System.Drawing.Size(243, 28);
             label1.TabIndex = 31;
@@ -363,7 +215,7 @@
             // 
             label13.AutoSize = true;
             label13.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            label13.Location = new System.Drawing.Point(8, 491);
+            label13.Location = new System.Drawing.Point(337, 137);
             label13.Name = "label13";
             label13.Size = new System.Drawing.Size(223, 28);
             label13.TabIndex = 32;
@@ -373,7 +225,7 @@
             // 
             labelConsume.AutoSize = true;
             labelConsume.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            labelConsume.Location = new System.Drawing.Point(267, 448);
+            labelConsume.Location = new System.Drawing.Point(596, 94);
             labelConsume.Name = "labelConsume";
             labelConsume.Size = new System.Drawing.Size(52, 28);
             labelConsume.TabIndex = 33;
@@ -383,7 +235,7 @@
             // 
             labelGenerate.AutoSize = true;
             labelGenerate.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            labelGenerate.Location = new System.Drawing.Point(267, 491);
+            labelGenerate.Location = new System.Drawing.Point(596, 137);
             labelGenerate.Name = "labelGenerate";
             labelGenerate.Size = new System.Drawing.Size(45, 28);
             labelGenerate.TabIndex = 34;
@@ -393,7 +245,7 @@
             // 
             label14.AutoSize = true;
             label14.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            label14.Location = new System.Drawing.Point(381, 471);
+            label14.Location = new System.Drawing.Point(670, 94);
             label14.Name = "label14";
             label14.Size = new System.Drawing.Size(127, 28);
             label14.TabIndex = 35;
@@ -404,18 +256,28 @@
             labelNet.AutoSize = true;
             labelNet.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             labelNet.ForeColor = System.Drawing.Color.IndianRed;
-            labelNet.Location = new System.Drawing.Point(514, 471);
+            labelNet.Location = new System.Drawing.Point(790, 94);
             labelNet.Name = "labelNet";
             labelNet.Size = new System.Drawing.Size(40, 28);
             labelNet.TabIndex = 36;
             labelNet.Text = "net";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            label10.Location = new System.Drawing.Point(8, 94);
+            label10.Name = "label10";
+            label10.Size = new System.Drawing.Size(323, 28);
+            label10.TabIndex = 19;
+            label10.Text = "Average Daily Comsumption (kWh):";
             // 
             // DailyForecast
             // 
             AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             AutoSize = true;
-            ClientSize = new System.Drawing.Size(963, 528);
+            ClientSize = new System.Drawing.Size(932, 526);
             Controls.Add(labelNet);
             Controls.Add(label14);
             Controls.Add(labelGenerate);
@@ -430,30 +292,15 @@
             Controls.Add(labelSunset);
             Controls.Add(labelSunrise);
             Controls.Add(listBoxGenerators);
-            Controls.Add(radioButtonAll);
-            Controls.Add(radioButtonOne);
             Controls.Add(labelAverageDaily);
             Controls.Add(label10);
-            Controls.Add(label9);
-            Controls.Add(label8);
-            Controls.Add(label7);
-            Controls.Add(panelOther);
-            Controls.Add(panelMains);
-            Controls.Add(panelElectric);
             Controls.Add(checkBoxSmooth);
             Controls.Add(labelPanel);
-            Controls.Add(checkBoxAverage);
-            Controls.Add(label6);
-            Controls.Add(label5);
-            Controls.Add(label4);
-            Controls.Add(label3);
-            Controls.Add(label2);
             Controls.Add(canvas);
             Controls.Add(listBoxModelHouses);
             Controls.Add(labelCurrDay);
             Name = "DailyForecast";
             Text = "Daily Analysis";
-            Paint += DailyForecast_Paint;
             ResumeLayout(false);
             PerformLayout();
         }
@@ -463,24 +310,9 @@
         private System.Windows.Forms.ListBox listBoxModelHouses;
         private System.Windows.Forms.ListBox listBoxData;
         private System.Windows.Forms.Panel canvas;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.CheckBox checkBoxAverage;
         private System.Windows.Forms.Panel labelPanel;
         private System.Windows.Forms.CheckBox checkBoxSmooth;
-        private System.Windows.Forms.Panel panelElectric;
-        private System.Windows.Forms.Panel panelMains;
-        private System.Windows.Forms.Panel panelOther;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Label labelAverageDaily;
-        private System.Windows.Forms.RadioButton radioButtonOne;
-        private System.Windows.Forms.RadioButton radioButtonAll;
         private System.Windows.Forms.ListBox listBoxGenerators;
         private System.Windows.Forms.Label labelSunrise;
         private System.Windows.Forms.Label labelSunset;
@@ -496,5 +328,6 @@
         private System.Windows.Forms.Label labelGenerate;
         private System.Windows.Forms.Label label14;
         private System.Windows.Forms.Label labelNet;
+        private System.Windows.Forms.Label label10;
     }
 }
